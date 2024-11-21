c4715480c36ec2218c309848ae86ecfa000703ce
gabscadeu <00001096474761sp@al.educacao.sp.gov.br>
2024-11-21 11:18:21 -0300

    adicionando arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Escola',
+    'Qual é a minha sala?',
+    '3ºC'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o maior produtor de chocolate no mundo?',
+    'Costa do Marfim'
+)
+
+criaCartao(
+    'Programação',
+    'Quem é o professor de tecnologia?',
+    'O careca do Márcio'
+)
+
+criaCartao(
+    'Aulas',
+    'Quantas aulas de tecnologia temos por semana?',
+    '4'
+)
